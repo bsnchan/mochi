ef0ed1db2e1ce6a656acb3e3967a0cdf3869a258
Brenda Chan <brchan@pivotal.io>
2019-08-12 14:19:40 -0400

    updates when mochi is in next

--- scripts/mochiDateKnown.sh
+++ scripts/mochiDateKnown.sh
@@ -4,6 +4,6 @@ cf target -o piskandar -s development
 
 cf set-env mochi-app MOCHI.KNOWNNEXTIN true
 
-cf set-env mochi-app MOCHI.NEXTIN 2019-08-01
+cf set-env mochi-app MOCHI.NEXTIN 2019-08-14
 
 cf restart mochi-app
